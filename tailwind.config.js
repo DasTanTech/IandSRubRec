ca7c2fbee101280153c1fc85141ffc32e30972bb
DevZu <dastantech@gmail.com>
2023-05-09 05:06:42 -0500

    Node, Tailwind, FS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors :{
+        colorLight : '#7bc476',
+        colorBase : '#83bf44',
+        colorDark : '#008b48'
+      }
+    },
+  },
+  plugins: [],
+}
+
