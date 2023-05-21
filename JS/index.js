205b138f131ae435967d76241b0a0b30b08d94b2
DevZu <dastantech@gmail.com>
2023-05-21 04:19:50 -0500

    4:19 AM 05/21/2023

--- JS/index.js
+++ JS/index.js
@@ -14,3 +14,36 @@ mobNavCloseBtn.addEventListener('click', (e) => {
     mobNavSection.classList.add('hidden');
     document.body.classList.replace('overflow-hidden','overflow-auto');
 })
+
+
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+    var i;
+    
+    var sImages = document.getElementsByClassName("HCICont");
+
+    for (i = 0; i < sImages.length; i++) {
+        sImages[i].style.display = "none";
+    }
+    slidePosition++;
+    if (slidePosition > sImages.length) {slidePosition = 1}
+    sImages[slidePosition-1].style.display = "block";
+    setTimeout(SlideShow, 2000);   
+}
+
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("NavbarHome");
+    var mainSection = document.getElementById("BodyHome");
+  
+    if (window.pageYOffset > 0) {
+        navbar.classList.add("shadow-lg");
+        navbar.classList.remove("navbar-hidden");
+        mainSection.classList.remove("mt-16");
+      } else {
+        navbar.classList.remove("shadow-lg");
+        navbar.classList.add("navbar-hidden");
+        mainSection.classList.add("mt-16");
+      }
+  });
