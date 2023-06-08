4eaecc9a282d85e0444dfad26c7c68b0f6b8d13c
DasTanTech <32038878+DasTanTech@users.noreply.github.com>
2023-06-08 21:12:20 +0000

    06/08/2023 04:12 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,16 @@ module.exports = {
         colorLight : '#7bc476',
         colorBase : '#83bf44',
         colorDark : '#008b48',
-        colorContrast: '#00522b'
-      }
+        colorContrast: '#00522b',
+        colorContent: 'text-neutral-600',
+        colorPrimary: '#1fb622',
+        colorAccent: '#042a59',
+        colorDeAccent: '#4f6a8b'
+      },
+      fontFamily: {
+        title: ['Vollkorn', 'mono'],
+        content: ['Arial', 'sans-serif']
+      },
     },
   },
   plugins: [],
