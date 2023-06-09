a9ea191292fcb9488000c1f303bc7171c6c643dc
DasTanTech <32038878+DasTanTech@users.noreply.github.com>
2023-06-09 03:50:25 +0000

    06/08/2023 10:50 PM

--- JS/index.js
+++ JS/index.js
@@ -41,4 +41,26 @@ window.addEventListener("scroll", function() {
         headerInfoSec.classList.replace("w-96","w-48");
         
       }
-  });
+});
+
+
+
+window.onscroll = function() {
+    scrollFunction();
+};
+  
+function scrollFunction() {
+    var scrollToTopButton = document.getElementById("scrollToTopButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopButton.style.display = "block";
+    } else {
+      scrollToTopButton.style.display = "none";
+    }
+}
+  
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+  
+  
