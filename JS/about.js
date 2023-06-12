d8dc5e930fb8f42252127d6892dc0d26263e3015
DasTanTech <32038878+DasTanTech@users.noreply.github.com>
2023-06-12 07:03:50 +0000

    --

---
+++ JS/about.js
@@ -0,0 +1,16 @@
+var slidePosition = 0;
+SlideShow();
+
+function SlideShow() {
+    var i;
+    
+    var sImages = document.getElementsByClassName("HCICont");
+
+    for (i = 0; i < sImages.length; i++) {
+        sImages[i].style.display = "none";
+    }
+    slidePosition++;
+    if (slidePosition > sImages.length) {slidePosition = 1}
+    sImages[slidePosition-1].style.display = "block";
+    setTimeout(SlideShow, 1000);   
+}
